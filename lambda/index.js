543ac8802e167ef618ecb4612e301c647febb006
Tom Weston <weston.tom@gmail.com>
2024-02-04 21:15:58 +0000

    cleanup errors in 200 response body

--- lambda/index.js
+++ lambda/index.js
@@ -45,7 +45,7 @@ exports.handler = async (event) => {
 
     return {
         statusCode: 200,
-        body: JSON.stringify('Processed:' + item.Key),
+        body: JSON.stringify('Processed object successfully'),
     };
 };
 
